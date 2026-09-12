a11dea550906b63f4988a8e5d576eba53e9f5889
mynurkgz-ux <mynurkgz@gmail.com>
2026-09-12 13:55:50 +0600

    first commit

---
+++ one.js
@@ -0,0 +1,11 @@
+const a = document.querySelector('.a')
+
+const data = [
+    ]
+data.forEach((item) => {
+const divi = document.createElement('div')
+divi.innerHTML = `
+
+`
+a.appendChild(divi)
+})
